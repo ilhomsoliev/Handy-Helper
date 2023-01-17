71c2d7325358d0ec0603b692c089fdf28a511725
ilhomsoliev <solievoliver@gmail.com>
2023-01-17 16:34:40 +0500

    bottombar added

---
+++ app/src/main/java/com/ikcollab/handyhelper/navigation/bottomBar/BottomNavItem.kt
@@ -0,0 +1,15 @@
+package com.ikcollab.handyhelper.navigation.bottomBar
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.ikcollab.handyhelper.navigation.Screens
+
+sealed class BottomNavItem(var title:String, var icon:ImageVector, var screen_route:String){
+
+    object Goals : BottomNavItem("Goals", Icons.Default.Flag,Screens.GoalsScreen.route)
+    object ToDoList: BottomNavItem("To-Do list",Icons.Default.ListAlt,Screens.ChoresScreen.route)
+    object Tracker: BottomNavItem("Tracker",Icons.Default.Cached,Screens.TrackerScreen.route)
+    object Notes: BottomNavItem("Notes",Icons.Default.Note,Screens.NotesScreen.route)
+    object Budget: BottomNavItem("Budget",Icons.Default.AttachMoney,Screens.BudgetScreen.route)
+}
