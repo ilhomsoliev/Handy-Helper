d69d5f05057f72872bffbcceb632bbc27cfae260
ilhomsoliev <solievoliver@gmail.com>
2023-01-17 14:10:22 +0500

    Initial commit

---
+++ app/src/main/java/com/ikcollab/handyhelper/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ikcollab.handyhelper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ikcollab.handyhelper.ui.theme.HandyHelperTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HandyHelperTheme {
+
+            }
+        }
+    }
+}
