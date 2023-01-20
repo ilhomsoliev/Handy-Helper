54051a7bd199ac806c2a419faf4cd29c938fa57e
ilhomsoliev <solievoliver@gmail.com>
2023-01-20 08:29:09 +0500

    added daos for todo

--- data/local/src/main/java/com/ikcollab/local/db/HandyHelperDatabase.kt
+++ data/local/src/main/java/com/ikcollab/local/db/HandyHelperDatabase.kt
@@ -6,19 +6,25 @@ import com.ikcollab.local.dao.goals.GoalDao
 import com.ikcollab.local.dao.goals.StepGoalDao
 import com.ikcollab.local.dao.notes.FolderDao
 import com.ikcollab.local.dao.notes.NoteDao
+import com.ikcollab.local.dao.todo_list.TodoCategoryDao
+import com.ikcollab.local.dao.todo_list.TodoDao
 import com.ikcollab.model.local.goals.GoalEntity
 import com.ikcollab.model.local.goals.StepGoalEntity
 import com.ikcollab.model.local.note.FolderEntity
 import com.ikcollab.model.local.note.NoteEntity
+import com.ikcollab.model.local.todo_list.TodoCategoryEntity
+import com.ikcollab.model.local.todo_list.TodoEntity
 
 @Database(
-    entities = [NoteEntity::class,FolderEntity::class, StepGoalEntity::class, GoalEntity::class],
+    entities = [NoteEntity::class, FolderEntity::class, StepGoalEntity::class, GoalEntity::class, TodoCategoryEntity::class, TodoEntity::class],
     version = 1,
     exportSchema = false
 )
-abstract class HandyHelperDatabase:RoomDatabase() {
+abstract class HandyHelperDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
     abstract fun folderDao(): FolderDao
     abstract fun goalDao(): GoalDao
     abstract fun stepGoalDao(): StepGoalDao
+    abstract fun todoDao(): TodoDao
+    abstract fun todoCategoryDao(): TodoCategoryDao
 }
