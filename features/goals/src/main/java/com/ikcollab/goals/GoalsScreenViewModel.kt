cc4bb2946937db9c847e4ba5687ff11a4a069de0
ilhomsoliev <solievoliver@gmail.com>
2023-01-20 11:47:04 +0500

    goals screen v.1

---
+++ features/goals/src/main/java/com/ikcollab/goals/GoalsScreenViewModel.kt
@@ -0,0 +1,12 @@
+package com.ikcollab.goals
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GoalsScreenViewModel @Inject constructor(
+
+):ViewModel() {
+
+}
