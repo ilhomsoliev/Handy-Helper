590e388d4aa3b18c61752504b546e032f5f7e996
Ahrorovk <akhrorovk5758@gmail.com>
2023-01-22 00:32:27 +0500

    add deleteNote

--- features/notes/src/main/java/com/ikcollab/notes/presentation/foldersNotesScreen/FoldersNoteScreenViewModel.kt
+++ features/notes/src/main/java/com/ikcollab/notes/presentation/foldersNotesScreen/FoldersNoteScreenViewModel.kt
@@ -3,7 +3,9 @@ package com.ikcollab.notes.presentation.foldersNotesScreen
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ikcollab.domain.usecase.notes.note.DeleteNoteByIdUseCase
 import com.ikcollab.domain.usecase.notes.note.GetNotesByFolderIdUseCase
+import com.ikcollab.model.dto.note.NoteDto
 import com.ikcollab.model.dto.note.NoteState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
@@ -14,6 +16,7 @@ import javax.inject.Inject
 @HiltViewModel
 class FoldersNoteScreenViewModel @Inject constructor(
     private val getNotesByFolderIdUseCase: GetNotesByFolderIdUseCase,
+    private val deleteNoteByIdUseCase: DeleteNoteByIdUseCase
 ) : ViewModel() {
     private val _stateNotesByFolderId = mutableStateOf(NoteState())
     val stateNotesByFolderId = _stateNotesByFolderId
@@ -23,6 +26,17 @@ class FoldersNoteScreenViewModel @Inject constructor(
             getNotesByFolderIdUseCase(folderId = folderId).onEach {
                 _stateNotesByFolderId.value = _stateNotesByFolderId.value.copy(it)
             }.launchIn(viewModelScope)
+        }
+    }
+    fun deleteNoteById(
+        id:Int,
+        title:String,
+        description:String,
+        dateCreated:Long,
+        folderId:Int
+    ) {
+        viewModelScope.launch {
+            deleteNoteByIdUseCase(NoteDto(id, title, description, dateCreated, folderId))
         }
     }
 }
